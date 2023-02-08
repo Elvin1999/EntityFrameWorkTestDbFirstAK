fead302de6b3b415133b3cbf81461992944a1978
camalzadeelvin <camalzadeelvin@STHQ0127-03>
2023-02-08 16:34:09 +0400

    Add project files.

---
+++ EntityFrameWorkTestDbFirst/SQLQuery1.sql
@@ -0,0 +1,58 @@
+﻿CREATE DATABASE Library
+GO
+USE Library
+CREATE TABLE Authors(
+Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+[Name] NVARCHAR(30) NOT NULL,
+Surname NVARCHAR(50) NOT NULL,
+Age DATETIME2
+)
+GO
+
+INSERT INTO Authors([Name],[Surname],Age)
+VALUES('Linus','Torvalds',GETDATE()),
+('Leyla','Mammadova',GETDATE()),
+('Akif','Mammadli',GETDATE())
+
+GO
+
+CREATE TABLE Categories(
+Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+[Name] NVARCHAR(30) NOT NULL
+)
+
+GO
+INSERT INTO Categories([Name])
+VALUES('Dram'),('Adventure'),('Sci-Fi'),('Programming')
+
+GO
+
+CREATE TABLE Books(
+Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+[Name] NVARCHAR(30) NOT NULL,
+[Pages] INT NOT NULL,
+[AuthorId] INT FOREIGN KEY REFERENCES Authors(Id) ON DELETE CASCADE NOT NULL,
+[CategoryId] INT FOREIGN KEY REFERENCES Categories(Id) ON DELETE CASCADE NOT NULL
+)
+
+GO
+
+INSERT INTO Books([Name],[Pages],[AuthorId],[CategoryId])
+VALUES('Linus Essential',350,1,3),
+('C#',1200,2,4),
+('Tom Sawyer Adventure',70,3,2),
+('C++',350,1,3),
+('Design Patterns',350,2,4)
+
+
+
+--SP
+
+CREATE PROCEDURE sp_GetBookById
+@book_id INT
+AS
+BEGIN
+SELECT * FROM Books AS B
+WHERE B.Id=@book_id
+
+END
---
+++ EntityFrameWorkTestDbFirst/ViewModels/MainViewModel.cs
@@ -0,0 +1,143 @@
+﻿using EntityFrameWorkTestDbFirst.Entities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EntityFrameWorkTestDbFirst.ViewModels
+{
+    public class MainViewModel:BaseViewModel
+    {
+		private ObservableCollection<Book> allBooks;
+
+		public ObservableCollection<Book> AllBooks
+		{
+			get { return allBooks; }
+			set { allBooks = value; OnPropertyChanged(); }
+		}
+
+
+		public MainViewModel()
+		{
+			AllBooks = new ObservableCollection<Book>();
+			//CallAddAsync();
+			//UpdateAsync();
+			//RemoveAsync();
+			//GetAsync();
+
+			CallStoredProcedure();
+		}
+
+		private void CallStoredProcedure()
+		{
+            //using (var context=new LibraryEntities())
+            //{
+            //	var result=context.sp_GetBookById(4).First();
+            //	var author = context.Authors.SingleOrDefault(a => a.Id == result.AuthorId);
+            //	var book = new Book
+            //	{
+            //		 Id=result.Id,
+            //		  AuthorId=result.AuthorId,
+            //		  Author=author,
+            //		   CategoryId=result.CategoryId,
+            //		    Name=result.Name,
+            //			 Pages=result.Pages
+            //	};
+            //	AllBooks = new ObservableCollection<Book>
+            //	{
+            //		book
+            //	};
+            //         }
+
+            using (var context = new LibraryEntities())
+            {
+                var result = context.sp_GetBookAuthorCategoryById2(4).First();
+				MessageBox.Show($"{result.Book}  {result.Author}   {result.Category}");
+            }
+
+        }
+
+		private async void UpdateAsync()
+		{
+			using (var context=new LibraryEntities())
+			{
+				var book = await context.Books
+					.SingleOrDefaultAsync(b => b.Id == 5);
+				if (book != null)
+				{
+					book.Name = "I updated 3";
+					context.Entry(book).State = EntityState.Modified;
+					await context.SaveChangesAsync();
+				}
+                GetAsync();
+            }
+		}
+
+		private async void RemoveAsync()
+		{
+			using (var context=new LibraryEntities())
+			{
+				var deletedBook = await context
+					.Books
+					.FirstOrDefaultAsync(b => b.Id == 7);
+				if (deletedBook != null)
+				{
+					context.Entry(deletedBook).State = EntityState.Deleted;
+					await context.SaveChangesAsync();
+				}
+                GetAsync();
+            }
+		}
+
+		private async void CallAddAsync()
+		{
+			string data = await AddAsync();
+		}
+
+		private async Task<string> AddAsync()
+		{
+			using (var context=new LibraryEntities())
+			{
+				var book = new Book
+				{
+					Name="My New book",
+					AuthorId=1,
+					CategoryId=1,
+					Pages=1111
+				};
+				context.Entry(book).State= EntityState.Added;
+				return (await context.SaveChangesAsync()).ToString();
+			}
+		}
+
+		private async void GetAsync()
+		{
+			using (var context=new LibraryEntities())
+			{
+				var books = await context
+					.Books
+					.Include(nameof(Book.Author))
+					.Include(nameof(Book.Category))
+					.ToListAsync();
+
+
+				AllBooks = new ObservableCollection<Book>(books);
+
+				//var books = await context
+				//	.Books
+				//	.Include(nameof(Book. Author))
+				//	.Include(nameof(Book.Category))
+				//	.FirstOrDefaultAsync(b => b.Id == 1);
+
+    //            AllBooks = new ObservableCollection<Book>
+				//{
+				//	books,
+				//};
+			}
+		}
+	}
+}
